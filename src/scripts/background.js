2d1c65e45c6d62bdbceee831d562ec57fc3516fb
Amir Meimari <amirmeimari@gmail.com>
2020-05-10 20:09:42 +0430

    Add toggle functionality

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,1 +1,11 @@
-// chrome.runtime.reload()
+let toggle = true;
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  toggle = !toggle;  
+  chrome.browserAction.setIcon({path: (toggle ? "icons/icon_16x16.png" : "icons/icon_16x16.png")});
+  chrome.tabs.sendMessage(tab.id, toggle);
+});
+
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+  if (request.reload) callback(toggle);
+});
--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -1,2 +1,19 @@
+// insert google font into document head
 const fontLinkTag = `<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">`
 document.head.insertAdjacentHTML('beforeend', fontLinkTag)
+
+const toggleClass = (status) => {
+  console.log(status);
+  
+  if (status) {
+    document.body.classList.remove('bso-disabled')
+  } else {
+    document.body.classList.add('bso-disabled')
+  }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, cb) => {
+  toggleClass(request)
+})
+
+chrome.runtime.sendMessage({reload: true}, toggleClass)
