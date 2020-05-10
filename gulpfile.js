73247c2cc7e375051bbba426e56cc55d480ab240
Amir Meimari <amirmeimari@gmail.com>
2020-05-10 18:20:33 +0430

    Add project structure

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const VERSION = '1.0.0'
+const FILENAME = `better-stack-v${VERSION}.zip`
+
+const {src, dest, watch, series, parallel} = require('gulp')
+const connect = require('gulp-connect')
+const del = require('del')
+const path = require('path')
+const zip = require('gulp-zip')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const cleanCSS = require('gulp-clean-css')
+const autoprefixer = require('autoprefixer')
+
+sass.compiler = require('node-sass')
+
+const destination = (relativePath = '/') => {
+  return path.join('dist', relativePath)
+}
+
+const clean = () => {
+  return del(destination('/'))
+}
+
+const manifest = () => {
+  return src('manifest.json').pipe(dest(destination('/')))
+}
+
+const css = () => {
+  return src('./src/styles/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest(destination('/css')))
+    .pipe(connect.reload())
+}
+
+const script = () => {
+  return src('./src/scripts/toggle.js')
+    .pipe(dest(destination('/js')))
+    .pipe(connect.reload())
+}
+
+const background = () => {
+  return src('./src/scripts/background.js')
+  .pipe(dest(destination()))
+  .pipe(connect.reload())
+}
+
+const zipAll = () => {
+  return src('dist/**')
+    .pipe(zip(FILENAME))
+    .pipe(dest(destination('../build')))
+}
+
+exports.clean = clean
+exports.build = series(clean, parallel(manifest, css, script, background), zipAll)
+exports.dev = series(clean, parallel(manifest, css, script, background), (done) => {
+  connect.server({
+    root: destination('/'),
+    livereload: true,
+  })
+  watch('styles/*.scss')
+  watch('scripts/*.js')
+  done()
+})
